1eec185c661f4d4b4ee73ac07ea87e18aa2730a7
Andrew Hollis <thejester129@gmail.com>
2023-05-18 12:20:55 +0100

    added support for actions with args

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ if (len(devices) == 0):
     print("no devices discovered")
     exit()
 
+print("\n")
 print("choose device:")
 
 for idx, device in enumerate(devices):
@@ -16,6 +17,7 @@ for idx, device in enumerate(devices):
 chosen_index = input()
 device = devices[int(chosen_index)]
 
+print("\n")
 print("choose service:")
 
 for idx, service in enumerate(device.services):
@@ -25,6 +27,7 @@ chosen_service = input()
 full_service_id = device.services[int(chosen_service)].service_id
 service_id = full_service_id.split("serviceId:")[1]
 
+print("\n")
 print("choose action:")
 
 for idx, action in enumerate(device[service_id].actions):
@@ -34,4 +37,23 @@ chosen_action = input()
 action = device[service_id].actions[int(chosen_action)]
 action_name = action.name
 
-print(device[service_id][action_name]())
+action_to_invoke = device[service_id][action_name]
+action_args = action_to_invoke.argsdef_in
+
+if action_args == None or len(action_args) == 0:
+    print("\n")
+    print("invoking " + str(action_to_invoke))
+    result = action_to_invoke()
+    print(result)
+    exit()
+
+input_args = {}
+for arg in action_args:
+    print("\n")
+    print(str(arg[0]) + ": ")
+    input_arg = input()
+    input_args[str(arg[0])] = input_arg
+
+print("invoking action " + str(action_to_invoke))
+result = action_to_invoke(**input_args)
+print(result)
