78b47a0b56587ee6e22e01aa2513c1dccd699fb2
Andrew Hollis <thejester129@gmail.com>
2023-05-18 12:30:46 +0100

    add service-action loop and README

---
+++ README.md
@@ -0,0 +1,39 @@
+upnp actions from the terminal
+
+`python3 run.py`
+
+$ discovering devices...
+
+$ choose device:
+
+0: Device 1
+
+1: Device 2
+
+$ choose service:
+
+0: Service 1
+
+1: Service 2
+
+2: Service 3
+
+$ choose action:
+
+0: Action 1
+
+1: Action 2
+
+2: Action 3
+
+Action 1 argument:
+
+$ arg1
+
+invoking action Action 1
+
+{'Value': 'Result'}
+
+$ choose service:
+
+...
--- run.py
+++ run.py
@@ -17,43 +17,49 @@ for idx, device in enumerate(devices):
 chosen_index = input()
 device = devices[int(chosen_index)]
 
-print("\n")
-print("choose service:")
 
-for idx, service in enumerate(device.services):
-    print(str(idx) + ": " + str(service.service_id))
-
-chosen_service = input()
-full_service_id = device.services[int(chosen_service)].service_id
-service_id = full_service_id.split("serviceId:")[1]
-
-print("\n")
-print("choose action:")
-
-for idx, action in enumerate(device[service_id].actions):
-    print(str(idx) + ": " + str(action))
-
-chosen_action = input()
-action = device[service_id].actions[int(chosen_action)]
-action_name = action.name
-
-action_to_invoke = device[service_id][action_name]
-action_args = action_to_invoke.argsdef_in
-
-if action_args == None or len(action_args) == 0:
+def do_service_action_loop():
     print("\n")
-    print("invoking " + str(action_to_invoke))
-    result = action_to_invoke()
+    print("choose service:")
+
+    for idx, service in enumerate(device.services):
+        print(str(idx) + ": " + str(service.service_id))
+
+    chosen_service = input()
+    full_service_id = device.services[int(chosen_service)].service_id
+    service_id = full_service_id.split("serviceId:")[1]
+
+    print("\n")
+    print("choose action:")
+
+    for idx, action in enumerate(device[service_id].actions):
+        print(str(idx) + ": " + str(action))
+
+    chosen_action = input()
+    action = device[service_id].actions[int(chosen_action)]
+    action_name = action.name
+
+    action_to_invoke = device[service_id][action_name]
+    action_args = action_to_invoke.argsdef_in
+
+    if action_args == None or len(action_args) == 0:
+        print("\n")
+        print("invoking " + str(action_to_invoke))
+        result = action_to_invoke()
+        print(result)
+        exit()
+
+    input_args = {}
+    for arg in action_args:
+        print("\n")
+        print(str(arg[0]) + ": ")
+        input_arg = input()
+        input_args[str(arg[0])] = input_arg
+
+    print("invoking action " + str(action_to_invoke))
+    result = action_to_invoke(**input_args)
     print(result)
-    exit()
+    do_service_action_loop()
 
-input_args = {}
-for arg in action_args:
-    print("\n")
-    print(str(arg[0]) + ": ")
-    input_arg = input()
-    input_args[str(arg[0])] = input_arg
 
-print("invoking action " + str(action_to_invoke))
-result = action_to_invoke(**input_args)
-print(result)
+do_service_action_loop()
